e193e300c22465281f73dbcea075eb69c51dcb59
Alexander Balakirev <balakirevs@i.ua>
2012-07-13 17:47:06 +0300

    Precompiled

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,14 @@ module RefApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.initialize_on_precompile = false
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+     
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
